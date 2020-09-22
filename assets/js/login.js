c54cbdcb06c7201e2a990fee5e4909122b3d9ab5
Rafael Mendes <mendesrafa.c@gmail.com>
2020-09-21 22:01:39 -0300

    adding login.js for log in requirements. working as intended

---
+++ assets/js/login.js
@@ -0,0 +1,50 @@
+var user = {};
+var access = {};
+
+function signIn () {
+    user.email = document.querySelector(".forms-container #email").value;
+    user.password = document.querySelector(".forms-container #password").value;
+    clearInput();
+    validateInput();
+}
+function clearInput () {
+    document.querySelector(".forms-container #email").value = '';
+    document.querySelector(".forms-container #password").value = '';
+}
+
+function validateInput () {
+    if (user.email === '' || user.password === '')
+        alert('Preencha todos os campos');
+    else { inputHandler(); }
+}
+
+function inputHandler () {
+    disableBtn();
+    const request = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v1/buzzquizz/users', user);
+    request.then(processToken).catch(errorHandler)
+}
+
+function errorHandler () {
+    alert("E-mail / senha incorretos");
+    enableBtn();
+}
+
+function processToken (response){
+    access.token = response.data.token;
+    changeScreen();
+}
+
+function changeScreen () {
+    const loginScreen = document.querySelector(".login-screen");
+    loginScreen.classList.add('hide-screen');
+}
+
+function disableBtn () {
+    var signInBtn = document.querySelector(".forms-container button");
+    signInBtn.setAttribute('disabled','');
+}
+
+function enableBtn () {
+    var signInBtn = document.querySelector(".forms-container button");
+    signInBtn.removeAttribute('disabled','');
+}
